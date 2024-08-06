a3c6ac6399d58bebda59f372189907a4f1f8a946
Tshwarelo365 <146174958+Tshwarelo365@users.noreply.github.com>
2024-08-06 17:53:32 +0200

    1st

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GourmetLoftWebsite
 This is a website that allows customers to order their food online 
+
+A Flask Web apllication that aims to help and reduce the amount of work or faced during peak our at Gourmet Loft, a place that sells fast food.
+
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/About')
+def About():
+    return render_template('About.html')
+
+@app.route('/Signin')
+def Signin():
+    return render_template('Signin.html')
+
+
+if __name__=="__main__":
+    
+    app.run(debug=True)
